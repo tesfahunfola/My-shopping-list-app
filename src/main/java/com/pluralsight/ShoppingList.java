57286eb24a6c15763e3307d47fb4f55a61f09961
Tesfahun Fola <folatesfahun@gmail.com>
2025-09-22 15:03:20 -0700

    My initial commit

---
+++ src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,18 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("My shopping List: ");
+        System.out.println("1. Banana");
+        System.out.println("2. Orange");
+        System.out.println("3. Bread");
+        System.out.println("4. cheese");
+        System.out.println("5. Napkin");
+        System.out.println("6. Socks");
+        System.out.println("7. Water");
+        System.out.println("8. Onions");
+        System.out.println("9. Garlic");
+        System.out.println("10. Flower");
+
+    }
+}
